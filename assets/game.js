c7103c213632585a228ad9556f3d5f619169070a
evangelina <evangelina.gomes@ellecktra.com>
2020-11-08 22:29:13 -0300

    test object-oriented programming

--- assets/game.js
+++ assets/game.js
@@ -18,6 +18,34 @@ window.requestAnimationFrame = (function () {
         };
 }());
 
+function Rectangle(x, y, width, height) {
+    
+    this.x = (x === undefined) ? 0 : x;
+    this.y = (y === undefined) ? 0 : y;
+    this.width = (width === undefined) ? 0 : width;
+    this.height = (height === undefined) ? this.width : height;
+
+    this.intersects = function (rect) {
+        if (rect == null) {
+            window.console.warn('Missing parameters on function intersects');
+        } else {
+            return (this.x < rect.x + rect.width &&
+                this.x + this.width > rect.x &&
+                this.y < rect.y + rect.height &&
+                this.y + this.height > rect.y);
+        }
+    };
+
+    this.fill = function (ctx) {
+        if (ctx == null) {
+            window.console.warn('Missing parameters on function fill');
+        } else {
+            ctx.fillRect(this.x, this.y, this.width, this.height);
+        }
+    };
+}
+
+
 function repaint() {
     window.requestAnimationFrame(repaint)
     paint(ctx);
@@ -42,6 +70,9 @@ function run() {
     }
 
     act(deltaTime);
+    var rect1 = new Rectangle(50, 50, 100, 60);
+
+
     /*act();*/
 }
 
